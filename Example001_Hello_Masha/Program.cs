b031c4ba99e9c19f5a9d8d5ff6b09134c0d21c08
Sergey Posokhov <posokhov_pit@mail.ru>
2022-10-30 11:29:13 +0300

    init: add Examples001 Hello Masha

---
+++ Example001_Hello_Masha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите Ваше имя ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, наконец-то Машутка с нами");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
